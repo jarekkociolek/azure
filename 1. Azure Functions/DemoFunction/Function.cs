435df6724629dbc6fb4ff19a306bd382e4c64bbe
jarekkociolek <jarekkociolek@gmail.com>
2021-03-15 18:01:41 +0100

    simple temperature function

--- 1. Azure Functions/DemoFunction/Function.cs
+++ 1. Azure Functions/DemoFunction/Function.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,23 +11,42 @@ namespace DemoFunction
 {
     public static class Function
     {
+        private const string OkStatus = "OK";
+        private const string CautionStatus = "CAUTION";
+        private const string DangerStatus = "DANGER";
+
         [FunctionName("Function")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            if (data.readings != null)
+            {
+                foreach (var reading in data.readings)
+                {
+                    if (reading.temperature <= 25)
+                    {
+                        reading.status = OkStatus;
+                    }
+                    else if (reading.temperature <= 50)
+                    {
+                        reading.status = CautionStatus;
+                    }
+                    else
+                    {
+                        reading.status = DangerStatus;
+                    }
+                }
+                return new OkObjectResult(data.readings);
+            }
 
+            string responseMessage = "Missing readings in body";
             return new OkObjectResult(responseMessage);
         }
     }
