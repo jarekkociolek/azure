0eda15f7be9f0908ffe92fb4191a6b7f65318851
jarekkociolek <jarekkociolek@gmail.com>
2021-04-15 18:11:15 +0200

    service bus startup code

---
+++ 2.messaging/ServiceBusQueuePublisher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace ServiceBusQueuePublisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            // Application code should start here.
+
+            await host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args);
+    }
+}
