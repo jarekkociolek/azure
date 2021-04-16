47762d5842f2188d798d53afe760f9c93c9a59be
jarekkociolek <jarekkociolek@gmail.com>
2021-04-16 18:56:11 +0200

    azure service bus topics

---
+++ 2.messaging/ServiceBusTopicPublisher/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceBusTopicPublisher
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+
+        static async Task Main(string[] args)
+        {
+            ConfigureServices(new ServiceCollection());
+
+            var connectionString = configuration.GetConnectionString("TopicConnectionString");
+            var topicName = configuration.GetValue<string>("TopicName");
+
+            var topicClient = new TopicClient(connectionString, topicName);
+            Console.WriteLine("Write a message:");
+            Console.WriteLine("");
+            var content = Console.ReadLine();
+
+            while (content != "exit")
+            {
+                var messageToSend = new Message(Encoding.UTF8.GetBytes(content));
+
+                try
+                {
+                    await topicClient.SendAsync(messageToSend);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                content = Console.ReadLine();
+            }
+
+            await topicClient.CloseAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+    }
+}
---
+++ 2.messaging/ServiceBusTopicSubscriber/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceBusTopicPublisher
+{
+    class Program
+    {
+        public static IConfigurationRoot configuration;
+        private static ISubscriptionClient _subscriptionClient;
+
+        static async Task Main(string[] args)
+        {
+            ConfigureServices();
+            var connectionString = configuration.GetConnectionString("TopicConnectionString");
+            var topic = configuration.GetValue<string>("TopicName");
+            var subscription = configuration.GetValue<string>("SubscriptionName");
+
+            _subscriptionClient = new SubscriptionClient(connectionString, topic, subscription);
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            _subscriptionClient.RegisterMessageHandler(MessageReceived, messageHandlerOptions);
+
+            Console.ReadLine();
+            await _subscriptionClient.CloseAsync();
+        }
+
+        static async Task MessageReceived(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            await _subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static void ConfigureServices()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+        }
+    }
+}
